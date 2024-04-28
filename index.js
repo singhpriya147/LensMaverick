ea25e643babb6c141a7e0fa349980bc0492bb5c5
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-04-28 11:54:06 +0530

    remove some useless comment

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ navButtons.forEach((button, index) => {
   });
 });
 
-// Automatically advance to the next slide every 3 seconds (adjust as needed)
+
 setInterval(() => {
   currentSlideIndex = (currentSlideIndex + 1) % slides.length;
   showSlide(currentSlideIndex);
@@ -35,53 +35,19 @@ setInterval(() => {
 
 
 
-// const images = [
-//   './assets/camila-cordeiro-haRyBAihS_0-unsplash.jpg',
-//   './assets/hiroko-yoshii-9y7y26C-l4Y-unsplash.jpg',
-//   './assets/rob-wingate-IlUqSRJYp8c-unsplash.jpg',
-// ];
-
-// const reviews = [
-//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Review 1',
-//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Review 2',
-//   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Review 3',
-//   // Add more reviews as needed
-// ];
-
-// let currentIndex = 0;
-// const imageElement = document.querySelector('.carousel-image');
-// const reviewElement = document.getElementById('reviewText');
-// const prevButton = document.getElementById('prevBtn');
-// const nextButton = document.getElementById('nextBtn');
-
-// function updateCarousel() {
-//   imageElement.src = images[currentIndex];
-//   reviewElement.textContent = reviews[currentIndex];
-// }
-
-// prevButton.addEventListener('click', () => {
-//   currentIndex = (currentIndex - 1 + images.length) % images.length;
-//   updateCarousel();
-// });
-
-// nextButton.addEventListener('click', () => {
-//   currentIndex = (currentIndex + 1) % images.length;
-//   updateCarousel();
-// });
-
 
 const testimonials = [
   {
     image: './assets/camila-cordeiro-haRyBAihS_0-unsplash.jpg',
     review:
       'Absolutely thrilled with our wedding photos! The photographer captured every special moment beautifully. The attention to detail and creativity really shines through in the final shots. Highly recommend their services for anyone looking for stunning wedding photography!',
-    user: 'jason derulo',
+    user: 'Jasmine Rivera',
   },
   {
     image: './assets/farid-askerov-F1uKnHlRzDc-unsplash.jpg',
     review:
       'Could not  be happier with my personal photoshoot experience! The photographer was incredibly professional and made me feel comfortable throughout the session.Th photos turned out amazing, capturing my personality perfectly. I will  definitely cherish these memories forever. Highly recommend their talent for any personal photoshoot',
-    user: 'jack huberman',
+    user: 'Lucas Thompson',
   },
   {
     image: './assets/hiroko-yoshii-9y7y26C-l4Y-unsplash.jpg',
@@ -89,7 +55,7 @@ const testimonials = [
       'An unforgettable scuba photoshoot experience! The photographer expertly captured the magic of the underwater world, from vibrant coral reefs to mesmerizing marine life. The photos are like a dream come true, showcasing the beauty and adventure of scuba diving. Highly recommend this unique photoshoot for anyone looking to capture their underwater adventures in stunning detail!',
     user: 'mike harvey',
   },
-  // Add more testimonial objects as needed
+ 
 ];
 
 let currentIndex = 0;
